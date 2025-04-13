0adaed3f9fa9465320c5623e7ca87e01012fac36
dleogml <dleogmlrnt@naver.com>
2025-04-13 15:06:38 +0900

    add: Facade 패턴

---
+++ src/main/java/org/example/section2/facade/after/EmailSettings.java
@@ -0,0 +1,14 @@
+package org.example.section2.facade.after;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
